7a009c1e7b783be8ff090d839780e5dc68a5d7f4
Ben Lee <ben@aircooledafe.com>
2026-09-18 11:02:35 +0100

    revert changes

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-# @app.route("/", methods=["GET"])
-# def home():
-#     return render_template("index.html")
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
 
 
 @app.route("/", methods=["GET"])
