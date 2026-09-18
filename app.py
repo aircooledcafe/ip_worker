3dd8e81e67d724934b4fc41d00cfb5493dffb7e6
Ben Lee <ben@aircooledafe.com>
2026-09-18 11:24:24 +0100

    testing

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+import asgiref.wsgi
+from asgi_tools import Request as ASGIRequest  # or a WSGI-to-ASGI wrapper
 from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def home():
-    #return render_template("index.html")
+    # return render_template("index.html")
     return "Hello World Test"
 
 
@@ -13,10 +16,17 @@ def return_ip():
     client_info = {
         "client_ip": request.remote_addr,
         "user_agent": request.user_agent.string,
-        "header": dict(request.headers)
+        "header": dict(request.headers),
     }
     return jsonify(client_info)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0")
+asgi_app = asgiref.wsgi.WsgiToAsgi(app)
+
+
+async def on_fetch(request, env):
+    import open_async_endpoint  # adapter mechanism
+
+    return await asgi_app(request)
