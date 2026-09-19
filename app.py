edc0c1e425b65c70d51efafd99af8dbec0be5c0a
Ben Lee <ben@aircooledafe.com>
2026-09-19 14:55:23 +0100

    split verbose output out

--- app.py
+++ app.py
@@ -13,13 +13,21 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def return_ip():
     client_info = {
+        "client_ip": request.remote_addr,
+        "cf-connecting-ip": request.headers.get("Cf-Connecting-Ip"),
+        "x-real-ip": request.headers.get("X-Real-Ip"),
+    }
+    return jsonify(client_info)
+
+@app.route("/verbose", methods=["GET"])
+def return_verbose():
+    client_info = {
         "client_ip": request.headers.get("Cf-Connecting-Ip"),
         "user_agent": request.user_agent.string,
         "x-real-ip": request.headers.get("X-Real-Ip"),
         "header": dict(request.headers),
     }
     return jsonify(client_info)
-
 
 # if __name__ == "__main__":
 #     app.run(host="0.0.0.0")
